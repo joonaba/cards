60e2d69984d20ac4e7a0e9084fb78fe541685899
pekkaonbae <joona.kokkonen@myy.haaga-helia.fi>
2019-03-14 02:06:00 +0200

    deletemuutoksia tehtry

--- list.php
+++ list.php
@@ -43,5 +43,35 @@ if($_SESSION['username'] == 'username') {
  print("No access.");
 }
 ?>
-
+<?php
+session_start();
+if($_SESSION['username'] == 'username') {
+ include "connect.php";
+ print("<html>");
+ $conn = connect_db();
+ $sql = "SELECT * FROM Addresses";
+ $result = mysqli_query($conn, $sql);
+ if (mysqli_num_rows($result) > 0) {
+ // output data of each row
+ while($row = mysqli_fetch_assoc($result)) {
+ echo $row["id"] . " ";
+ echo $row["name"] . " ";
+ echo $row["street_address"] . " ";
+ echo $row["zip"] . " ";
+ echo $row["city"] . " ";
+ echo $row["state"] . " ";
+ echo $row["country"] . " ";
+ echo "<a href=\"delete.php?id=" . $row["id"] . "\">DELETE</a>";
+ print("<br>");
+ }
+ } else {
+ echo "0 results";
+ }
+ print("<br><br>");
+ print("</html>");
+ mysqli_close($conn);
+} else {
+ print("No access.");
+}
+?>
 
